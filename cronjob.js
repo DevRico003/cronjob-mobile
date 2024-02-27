712ef15b08dd7373f78a6b30a2c909e726ca8074
Enrico Carteciano <134707381+DevRico003@users.noreply.github.com>
2024-02-27 14:40:59 +0100

    Update cronjob.js

--- cronjob.js
+++ cronjob.js
@@ -1,49 +1,45 @@
-// Using ES6 import instead of require
 import fetch from 'node-fetch';
 
-// URL to send the GET request to
 const url = 'https://www.carcenter-erding.de/api/mobile';
 
-// Function to execute the GET request with retry logic
-const fetchDataWithRetry = async (url, retries = 20, delay = 5000) => {
+const fetchDataWithRetry = async (url, retries = 9999, delay = 5000) => {
   let lastError;
 
   for (let attempt = 1; attempt <= retries; attempt++) {
     try {
-      // Sending the GET request
       const response = await fetch(url);
 
-      // Checking if the request was successful
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      // Parsing the response as JSON
       const data = await response.json();
 
-      // Outputting the data
-      console.log(data);
-      return; // Successfully fetched data, exit function
+      if (data.length === 0) {
+        console.error(`Attempt ${attempt}: Fetched data is an empty array, retrying...`);
+        lastError = new Error('Fetched data is an empty array');
+      } else {
+        console.log(data); // Data fetched successfully and is not an empty array
+        return;
+      }
     } catch (error) {
       console.error(`Attempt ${attempt}: Error fetching data`, error.message);
       lastError = error;
 
       // If the error is not a 504 (Gateway Timeout), break and don't retry
       if (!error.message.includes('504')) break;
+    }
 
-      // Waiting for a specified delay before retrying
-      if (attempt < retries) {
-        console.log(`Waiting ${delay}ms before retry...`);
-        await new Promise(resolve => setTimeout(resolve, delay));
-      }
+    // Waiting for a specified delay before retrying
+    if (attempt < retries) {
+      console.log(`Waiting ${delay}ms before retry...`);
+      await new Promise(resolve => setTimeout(resolve, delay));
     }
   }
 
-  // After all attempts, if data is still not fetched, throw the last encountered error
-  throw lastError;
+  throw lastError; // After all attempts, if a satisfactory response is not fetched, throw the last encountered error
 };
 
-// Invoking the function
 fetchDataWithRetry(url)
   .then(() => console.log('Data fetch successful'))
   .catch(error => console.error('Final error after retries:', error.message));
